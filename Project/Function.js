85fdd0bc2a44b3e507372a034a7fc19c6a4c26d8
Pankti Shah <pankti.shah@diamnow.com>
2022-06-13 23:37:57 +0530

    radio,select and checkbox validation and constants.js file

--- Project/Function.js
+++ Project/Function.js
@@ -1,4 +1,4 @@
-
+const _=require('lodash')
 const express = require('express');
 const app = express();
 var fs = require('fs');
@@ -6,6 +6,7 @@ const util = require('util')
 const makeDir = util.promisify(fs.mkdir)
 const makeFile = util.promisify(fs.writeFile)
 var path = require('path');
+const { type } = require('os');
 const staticPath = path.join(__dirname, './public/');
 app.use(express.static(staticPath));
 app.set('view engine', 'ejs');
@@ -29,20 +30,44 @@ function removeDir(projectFolderPath) {
 
 }
 
-function DataTypeConverion(data) {
-  console.log(data.required)
+function ModelAttributes(data) {
+
+  if(data.length==1 && (_.first(data).attr.type=="text"||_.first(data).attr.type=="number"||_.first(data).attr.type=="email")){
+     data=_.first(data);
+  }
+  else if(_.first(data).attr.type=='radio'){
+     _.first(data).attr["values"]= data.map((x)=>{return x.attr.value});
+    data=_.first(data);
+  }
+  else if(_.first(data).attr.type=='checkbox'){ 
+    _.first(data).attr["values"]= data.map((x)=>{return x.attr.value});
+   data=_.first(data);
+ }
+  else if(_.first(data).tag=="select"){
+    _.first(data).attr["values"]= _.first(data).child.filter((x)=>{return x.attr}).map((y)=>y.attr.value);
+    data=_.first(data);
+    data['attr']['type']="select";
+  }
+
   var typeDict = {
     "text": "String",
     "number":"Number",
-    "email":"String"
+    "email":"String",
+    "date":"Date",
+    "radio":"String",
+    "select":"String",
+    "checkbox":(data.attr.type=="checkbox" && data.attr.values.length>1)?"[String]":"Boolean"
   }
-  if (typeDict[data.type]) {
+  if (typeDict[data.attr.type]){
     obj={
-      type:typeDict[data.type],
-      ...(data.required!=undefined) &&{required:`[true,"${data.name} is required"]`},
-      ...(data.min!=undefined && data.min) && {min:`[${data.min},"${data.name} must be at leat ${data.min}"]`},
-      ...(data.min!=undefined && data.max) && {max:`[${data.max},"${data.name} must be up to ${data.max}"]`},
-      ...(data.type=='email')&&{match: `[new RegExp(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/), "Please enter a valid email address"]`}
+      type:typeDict[data.attr.type],
+      ...(data.attr.required!=undefined) &&{required:`[true,"${data.attr.name} is required"]`},
+      ...(data.attr.min!=undefined && data.attr.min) && {min:`[${data.min},"${data.attr.name} must be at least ${data.attr.min}"]`},
+      ...(data.attr.min!=undefined && data.attr.max) && {max:`[${data.attr.max},"${data.attr.name} must be up to ${data.attr.max}"]`},
+      ...(data.attr.type=='email')&&{match: `[new RegExp(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/), "Please enter a valid email address"]`},
+      ...(data.attr.type=='radio'||data.attr.type=='select'||(data.attr.type=='checkbox' && data.attr.values.length>1))&&{enum:`{values:[${"'" + data.attr.values.join("','") + "'"}],message:"Please choose ${data.attr.name.replace("[", "").replace("]","")}  from ${data.attr.values}"}`}
+    
+    
     }
     return obj;
   }
@@ -53,49 +78,41 @@ function DataTypeConverion(data) {
 };
 
 function createModel(projectFolderPath, model, modelProperty) {
-  makeDir(dirModel = `${projectFolderPath}/Model`).then(() => {
-    console.log(`Directory ${dirModel} is created`)
-
     app.render("Model", { model: model, modelProperty: modelProperty }, (err, res) => {
       if (err) console.log("Error occur in model creation");
-      makeFile(`${dirModel}/${model}.js`, res).then(() => {
+      makeFile(`${projectFolderPath}/Model/${model}.js`, res).then(() => {
         console.log("Model Created");
 
       }).catch((err) => { console.log("Error occur in model creation") });
     })
 
 
-  }).catch(err => { console.log(err.message); return `Model creation issue` })
-}
+  }
 
 function createController(projectFolderPath, title,) {
-  makeDir(dirController = `${projectFolderPath}/Controller`).then(() => {
-    console.log(`Directory ${dirController} is created`)
-
+  
     app.render("Controller", { title: title }, (err, res) => {
       if (err) console.log("Error occur in model creation");
-      makeFile(`${dirController}/${title}Controller.js`, res).then(() => {
+      makeFile(`${projectFolderPath}/Controller/${title}Controller.js`, res).then(() => {
         console.log("Controller Created");
       }).catch((err) => { console.log("Error occur in Controller creation") });
     })
 
 
-  }).catch(err => { console.log(err.message); return `Controller creation issue` })
+  
 }
 
 function createRoute(projectFolderPath, title,) {
-  makeDir(dirRoute = `${projectFolderPath}/Route`).then(() => {
-    console.log(`Directory ${dirRoute} is created`)
-
+ 
     app.render("Route", { title: title }, (err,res) => {
       if (err) console.log("Error occur in route creation");
-      makeFile(`${dirRoute}/${title}Route.js`, res).then(() => {
+      makeFile(`${projectFolderPath}/Route/${title}Route.js`, res).then(() => {
         console.log("Route Created");
       }).catch((err) => { console.log("Error occur in route creation") });
     })
 
 
-  }).catch(err => { console.log(err.message); return `Route creation issue` })
+ 
 }
 function createIndex(projectFolderPath,title) {
   
@@ -123,4 +140,30 @@ function createConnection(projectFolderPath) {
     }).catch((err) => { console.log("Error occur in connection.js creation") });
   })
 }
-module.exports = { removeDir, DataTypeConverion, createModel, createController, createRoute,createIndex,createPackage,createConnection};
+function  createConstants(projectFolderPath,rawData) {
+  var constatntDist={}
+  Object.keys(rawData).forEach((key)=>{
+    if(rawData[key].length>1 && !rawData[key].child){
+      constatntDist[_.first(rawData[key]).attr.name.toUpperCase().replace("[", "").replace("]","")]=rawData[key].map((x)=>{return `"${x.attr.value}"`});
+    }
+    else if(_.first(rawData[key]).child){
+      constatntDist[_.first(rawData[key]).attr.name.toUpperCase().replace("[", "").replace("]","")]= _.first(rawData[key]).child.filter((x)=>{return x.attr}).map((y)=>`"${y.attr.value}"`);
+    }
+  })
+  app.render("Constants",{constants:constatntDist},(err,res)=>{
+    if(err) console.log(err.message)
+    makeFile(`${projectFolderPath}/constants.js`,res).then(()=>{
+      console.log("constantjs created")
+    }).catch((err) => { console.log("Error occur in constant.js creation") });    
+  })
+}
+function createDir(projectFolderPath){
+  var folders=["Model","Controller","Route"];
+  _.map((folders),(x)=>{
+    makeDir(dirRoute = `${projectFolderPath}/${x}`).catch((err)=>console.log(err))
+  })
+    
+
+}
+
+module.exports = { removeDir,createDir, ModelAttributes, createModel, createController, createRoute,createIndex,createPackage,createConnection,createConstants};
--- Project/index.js
+++ Project/index.js
@@ -9,19 +9,18 @@ var html2json = require('html2json').html2json;
 const rmdir = require('rimraf');
 var bodyParser = require('body-parser');
 let mongoose = require('mongoose');
-var Constants = require('constants');
+var Constants = require('./constant');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 require('./connection')
 const functions = require("./Function.js")
-let constants = require('./constant');
 const cheerio = require('cheerio');
 const staticPath = path.join(__dirname, './public/');
 app.use(express.static(staticPath));
 app.set('view engine', 'ejs');
 const makeDir = util.promisify(fs.mkdir)
 const makeFile = util.promisify(fs.writeFile)
-
+const _=require('lodash')
 
 function genrateNode(htmlFile) {
   var htmlPath = path.join(__dirname, '.', 'HTMLSource', htmlFile);
@@ -30,16 +29,20 @@ function genrateNode(htmlFile) {
 
     let $ = cheerio.load(dataHtml);
     //res.send($.html("input,select,title"))
-    var htmlJson = html2json($.html("input,radio,title"));
+    var htmlJson = html2json($.html("input,radio,title,select,checkbox"));
 
     json = htmlJson.child
     var title = json.filter(obj => { return obj.tag == "title" });
     title = title.map(ele => { return ele.child[0].text })
     title = title[0].replace(/\s/g, "");
+    /**
+    //  * get body element 
+     */
     var body = json.filter(obj => { return obj.tag != "title" })
-    //var bodyForm=body[0].child.filter(obj=>{return obj.tag=="form"})
-    bodyForm = body.filter(obj => { return (obj.node == "element" && obj.attr && obj.attr.name) })
-    var modelProperty = typeConversion(bodyForm.map(obj => { return obj.attr }))
+    var rawModel = body.filter(obj => { return (obj.node == "element" && obj.attr && obj.attr.name) })
+    rawModel=rawModel.map(obj => { return{attr:obj.attr,child:obj.child||null,tag:obj.tag}})
+    var modelProperty = createModelProperty(_.groupBy(rawModel,'attr.name'))
+   
     var projectFolder = path.parse(path.basename(htmlPath)).name
     const projectFolderPath = path.join(__dirname, '../ProjectsDestination', projectFolder);
 
@@ -56,14 +59,16 @@ function genrateNode(htmlFile) {
     //create project folder
     var dir;
     makeDir(dir = projectFolderPath).then(() => {
-      console.log(`Directory ${dir} is created`)
+      functions.createDir(dir);
+      // console.log(`Directory ${dir} is created`)
       functions.createModel(dir, title, modelProperty);
       functions.createController(dir, title);
       functions.createRoute(dir,title);
       functions.createIndex(dir,title);
       functions.createPackage(dir,title);
-      functions.createConnection(dir)
-    }).catch(err => { console.log(err.message) })
+      functions.createConnection(dir);
+      functions.createConstants(dir,_.groupBy(rawModel,'attr.name'))
+   }).catch(err => { console.log(err.message) })
   })
 }
 
@@ -107,10 +112,11 @@ app.listen(PORT, () => {
 
 
 
-function typeConversion(data) {
+function createModelProperty(data) {
   var dict = {};
-  for (var i = 0; i < data.length; i++) {
-    dict[data[i].name] = functions.DataTypeConverion(data[i])
-  }
+  Object.keys(data).forEach((key)=>{
+    
+   dict[key.replace("[", "").replace("]","")] = functions.ModelAttributes(data[key])
+  })
   return dict;
 }
